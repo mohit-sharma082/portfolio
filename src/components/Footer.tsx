78d93753f7154d79cbbae727ee4cffa30d12b31d
Mohit <mohit.sharma@mogiio.com>
2023-10-28 16:40:56 +0530

    working

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer>
+			<div className="h-40 min-w-[50vw] p-10">Footer</div>
+		</footer>
+	);
+};
+
+export default Footer;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Menu, AlignLeft } from 'lucide-react';
 import { Button, buttonVariants } from './ui/button';
 import { useMediaQuery } from '@/util/useMediaQuery';
@@ -13,23 +13,26 @@ import {
 import { ModeToggle } from './ModeToggle';
 
 export default function Nav() {
-	const [toggled, setToggled] = useState(false);
 	const match = useMediaQuery('(min-width:1080px)');
 
 	return (
-		<nav className="relative flex justify-between items-center lg:px-20 lg:py-10">
+		<nav className="relative flex justify-between items-center py-3  min-w-[90vw] backdrop-blur-lg">
 			<div className="p-4">M.S.</div>
-
-			<ModeToggle />
-
-			<Sheet>
-				<SheetTrigger>{Hamburger()}</SheetTrigger>
-				<SheetContent>
-					<SheetHeader>
-						<SheetDescription>{Links()}</SheetDescription>
-					</SheetHeader>
-				</SheetContent>
-			</Sheet>
+			<div className="flex justify-between items-center">
+				{match ? (
+					Links(
+						'flex gap-7 bottom-0 left-0 w-full items-center justify-center pr-20'
+					)
+				) : (
+					<Sheet>
+						<SheetTrigger>{Hamburger()}</SheetTrigger>
+						<SheetContent>
+							<SheetHeader>{Links()}</SheetHeader>
+						</SheetContent>
+					</Sheet>
+				)}
+				<ModeToggle />
+			</div>
 		</nav>
 	);
 
@@ -37,25 +40,30 @@ export default function Nav() {
 		return (
 			<div className="m-5 relative group">
 				<Menu className="hidden group-hover:block" />
-				<AlignLeft className="block group-hover:hidden"/>
+				<AlignLeft className="block group-hover:hidden" />
 			</div>
 		);
 	}
 
-	function Links() {
+	function Links(styles?: string) {
+		/** default classes given for sidebar */
+		const classes =
+			styles ??
+			'flex flex-col gap-12 bottom-0 left-0 w-full h-screen items-center justify-center';
+
 		return (
-			<ul className="flex flex-col gap-12 bottom-0 left-0 w-full h-screen items-center justify-center">
+			<ul className={classes}>
 				<li>
-					<Button>Home</Button>
+					<Button variant={'link'}>Home</Button>
 				</li>
 				<li>
-					<Button>Projects</Button>
+					<Button variant={'link'}>Projects</Button>
 				</li>
 				<li>
-					<Button>About</Button>
+					<Button variant={'link'}>About</Button>
 				</li>
 				<li>
-					<Button>Contact</Button>
+					<Button variant={'link'}>Contact</Button>
 				</li>
 			</ul>
 		);
