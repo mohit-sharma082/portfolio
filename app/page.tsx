510fbdac3b992d6c25cfeca87896f7c83a607ac4
Mohit <116288240+mohit-sharma082@users.noreply.github.com>
2025-03-16 22:58:21 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,6 @@ import SectionWrapper from "@/components/section-wrapper";
 import { data } from "@/data/index";
 import PageWrapper from "@/components/page-wrapper";
 import { Button } from "@/components/ui/button";
-const skills = data.skills;
-const projects = data.projects;
-const bio = data.bio;
-const contact = data.contact;
-const experience = data.experience;
-const name = data.name;
-const profileImage = data.profileImage;
-
 
 const formatDate = (str: string) => {
   const date = new Date(str)
@@ -23,6 +15,16 @@ const formatDate = (str: string) => {
 }
 
 export default function Home() {
+  
+const skills = data.skills;
+const projects = data.projects;
+const bio = data.bio;
+const contact = data.contact;
+const experience = data.experience;
+const name = data.name;
+const profileImage = data.profileImage;
+
+
   return (
     <PageWrapper>
       <div className="flex flex-col gap-16 items-center w-full min-h-screen py-8 px-4">
